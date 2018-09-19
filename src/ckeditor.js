98669b648e6b0ff9bb5c7d8bfc7be507cefb3d5e
Panagis Tselentis <p.tselentis@ime.com.gr>
2018-09-19 16:07:41 +0300

    ref

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -26,6 +26,7 @@ import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import Widget from '@ckeditor/ckeditor5-widget/src/widget';
+import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed'
 
 export default class BalloonEditor extends BalloonEditorBase {}
 
@@ -49,7 +50,8 @@ BalloonEditor.builtinPlugins = [
 	Paragraph,
   Alignment,
   Table, TableToolbar,
-  Widget
+  Widget,
+  MediaEmbed
 ];
 
 // Editor configuration.
